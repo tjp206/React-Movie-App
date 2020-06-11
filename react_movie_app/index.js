1578cf43cc195d8a29ca92e43539864f5bfd0d4d
TJ <64153997+tjp206@users.noreply.github.com>
2020-06-11 14:44:19 +0100

    finished search function

--- react_movie_app/index.js
+++ react_movie_app/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import searchMovies from './searchMovies';
 
 class Main extends React.Component {
   render() {
     return (
         <div className="container">
       <h1 className="title">React Movie Search</h1>
+      <searchMovies/> 
       </div>
     );
   }
--- react_movie_app/searchMovies.js
+++ react_movie_app/searchMovies.js
@@ -3,9 +3,11 @@ import React from "react";
 export default function searchMovies() {
 
     return (
-        <form class="form"></form>
-        <label htmlFor="query" class="label">Movie Name</label>
-        <input type="text" name="query" placeholder="text">Enter movie name</input>
-        <button class="button" type="submit">Submit</button>
+        <form class="form">
+            <label htmlFor="query" class="label">Movie Name</label>
+            <input type="text" name="query" placeholder="i.e Jurassic Park"/>
+            <button class="button" type="submit">Search</button>
+        </form>
+       
     )
 }
