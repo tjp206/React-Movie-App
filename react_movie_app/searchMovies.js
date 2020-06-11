65b91e9c17e966dafa0a67b6251884542eec76af
TJ <64153997+tjp206@users.noreply.github.com>
2020-06-11 16:57:26 +0100

    display movie info

--- react_movie_app/searchMovies.js
+++ react_movie_app/searchMovies.js
@@ -25,6 +25,7 @@ export default function searchMovies() {
 }
 
     return (
+        <>
         <form class="form" onSubmit={searchMovies}>
             <label htmlFor="query" class="label">Movie Name</label>
             <input type="text" name="query"
@@ -33,6 +34,27 @@ export default function searchMovies() {
             />
             <button class="button" type="submit">Search</button>
         </form>
-       
+        <div className="card-list">
+            {movies.filter(movie => movie.poster_path).map(movie => (
+                <div className="card" key={movie.id}>
+                    <img className="card--image"
+                    src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/$
+                    {movie.poster_path}`}
+                    alt={movie.title + ' poster'}
+                    />
+                    <div className="card--content">
+                    <h3 className="card--title">{movie.title}</h3>
+                    <p><small>RELEASE DATE: {movie.release_date}</small></p>
+                    <p><small>RATING: {movie.vote_average}</small></p>
+                    <p className="card--desc">{movie.overview}</p>
+                    </div>
+
+
+                </div>
+            ))} 
+
+       </div>
+
+       </>
     )
 }
